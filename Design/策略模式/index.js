dba416f7829a278580c23cc0bb2ba8afd6581227
yushunwu <972774037@qq.com>
2020-06-17 22:22:15 +0800

    新增--策略模式

---
+++ Design/策略模式/index.js
@@ -0,0 +1,27 @@
+
+/**
+ * t:动画已消耗的时间
+ * b:小球原始位置
+ * c:小球目标位置
+ * d:动画持续的总时长
+ */
+var tween = {
+  linear: function(t, b, c, d) {
+    return c * t / d + b
+  },
+  easeIn: function(t, b, c, d) {
+    return c * (t /= d) * t + b
+  },
+  strongEaseIn: function(t, b, c, d) {
+    return c * (t /= d) * t * t * t * t +b
+  },
+  strongEaseOut: function (t, b, c, d) {
+    return c * ((t = t / d - 1) * t * t * t * t + 1) + b
+  },
+  sineaseIn: function (t, b, c, d) {
+    return c * (t /= d) * t * t + b
+  },
+  sineaseOut: function (t, b, c, d) {
+    return c * ((t = t / d -1) * t * t + 1) +b
+  }
+}
