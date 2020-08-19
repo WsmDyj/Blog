1d9a574802b77eb138289db61e9d69ecf98a517c
yushunwu <972774037@qq.com>
2020-08-19 13:37:47 +0800

    leetCoder

---
+++ leetcode/排序/647-回文子串.js
@@ -0,0 +1,55 @@
+// 给定一个字符串，你的任务是计算这个字符串中有多少个回文子串。
+// 具有不同开始位置或结束位置的子串，即使是由相同的字符组成，也会被视作不同的子串。
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+const isPalindrome = (s) => {
+  let i = 0
+  let j = s.length - 1
+  while (i < j) {
+    if (s[i] !== s[j]) return false
+    i++
+    j--
+  }
+  return true
+}
+var countSubstrings = function (s) {
+  let count = 0
+  for (let i = 0; i < s.length; i++) {
+    for (let j = i; j < s.length; j++) {
+      if (isPalindrome(s.substring(i, j + 1))) {
+        count++
+      }
+    }
+  }
+  return count
+}
+
+// console.log(countSubstrings('abc'))
+console.log(1); 
+setTimeout(function () { // 宏任务
+  console.log(2)
+}, 1000);
+new Promise(function (resolve) { 
+  console.log(3)
+  for (var i = 100; i > 0; i--) {
+    i == 1 && resolve(console.log(8))
+  }
+  console.log(4)
+  setTimeout(function () { // 宏任务
+    console.log(9)
+  }, 0);
+}).then(function () { // 微任务
+  console.log(5)
+}).then(function () { // 微任务
+  console.log(6)
+});
+console.log(7); // 宏任务
+ //1 7 3 4 5 6 2
+
+ // 1 3 4 7 5 6 2
+
+ // 134 8 7 562
