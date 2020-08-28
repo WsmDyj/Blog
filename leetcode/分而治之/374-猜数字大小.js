b993c0fb3fe44eac724e75f8e7dc046e88b78ff0
yushunwu <972774037@qq.com>
2020-08-28 21:18:19 +0800

    新增--leetcode-374

---
+++ leetcode/分而治之/374-猜数字大小.js
@@ -0,0 +1,15 @@
+var guessNumber = function (n) {
+  let rec= (low, high) => {
+    const mid = Math.floor((low + high) /2)
+    const res = guess(mid)
+    if (res === 0) {
+      return mid
+    } else if (res === 1) {
+      return rec(mid + 1, high)
+    } else {
+      return rec(1, mid - 1)
+    }
+  }
+  return rec(1, n)
+};
+console.log(guessNumber('a'))
