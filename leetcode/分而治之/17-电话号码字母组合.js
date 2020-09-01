6d68ffa0777fdd17f4ad89ce702c668ab86c10f0
yushunwu <972774037@qq.com>
2020-09-01 19:35:56 +0800

    leetcode-169

---
+++ leetcode/分而治之/17-电话号码字母组合.js
@@ -0,0 +1,9 @@
+var letterCombinations = function (digits) {
+  if(digits.length === 0) return []
+  const arr = []
+  const map = { '2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl', '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz' };
+  const dfs = () => {
+
+  }
+  
+};
