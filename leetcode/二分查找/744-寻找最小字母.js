280e8c48ff4d247b7619b73d70cd5e2720955089
yushunwu <972774037@qq.com>
2020-09-24 09:42:22 +0800

    fix: update leetcode-744

--- leetcode/二分查找/744-寻找最小字母.js
+++ leetcode/二分查找/744-寻找最小字母.js
@@ -14,14 +14,13 @@
  * @param {character} target
  * @return {character}
  * 寻找左边界，所以就考虑每次搜索的区间定义为 [left, right);
-当 letters[mid] <= target的时候，需要将left往右移动一位，
-当 letters[mid] > target的时候，由于是左闭右开的，right = mid
-当 left === right 的时候, 遍历完成， left 的含义就是，表示比 letters[left] 要小位置
-
+  当 letters[mid] <= target的时候，需要将left往右移动一位，
+  当 letters[mid] > target的时候，由于是左闭右开的，right = mid
+  当 left === right 的时候, 遍历完成， left 的含义就是，表示比 letters[left] 要小位置
  */
 
 var nextGreatestLetter = function (letters, target) {
-  let left = 0, right = target.length -1, mid;
+  let left = 0, right = letters.length -1, mid;
   while(left <= right) {
     mid = Math.floor((left + right) /2)
     if (letters[mid] <= target) {
@@ -30,7 +29,7 @@ var nextGreatestLetter = function (letters, target) {
       right = mid -1
     }
   }
-  return letters[left % letters.length];
-};
+  return letters[left % letters.length]
+}
 
-console.log(nextGreatestLetter(["c", "f", "j"], "c"))
+console.log(nextGreatestLetter(["c", "f", "j"], "f"))
