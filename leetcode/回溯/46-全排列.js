8b73eaf7df14a54853ea523878200c6403874b6d
yushunwu <972774037@qq.com>
2020-10-10 09:40:48 +0800

    feat: leetcode-46

---
+++ leetcode/回溯/46-全排列.js
@@ -0,0 +1,25 @@
+// 递归模拟出所有的情况
+// 遇到包含重复元素的情况就回溯
+// 收集所有到达递归终点的额情况，并返回
+
+var permute = function (nums) { 
+  let res = []
+  const backtrack = (path) => {
+    if (path.length === nums.length) {
+      res.push(path)
+      return
+    }
+    nums.forEach(n => {
+      if (path.includes(n)) {
+        return
+      }
+      backtrack(path.concat(n))
+    })
+  }
+  backtrack([])
+  return res
+}
+
+
+
+console.log(permute([]))
