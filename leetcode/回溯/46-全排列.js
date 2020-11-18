79fe97d6e9f7e5fe763658c65f8fc3c533629cf0
wusimin7 <972774037@qq.com>
2020-11-18 14:03:13 +0800

    feat: leetcode-46

--- leetcode/回溯/46-全排列.js
+++ leetcode/回溯/46-全排列.js
@@ -2,24 +2,46 @@
 // 遇到包含重复元素的情况就回溯
 // 收集所有到达递归终点的额情况，并返回
 
-var permute = function (nums) { 
-  let res = []
-  const backtrack = (path) => {
-    if (path.length === nums.length) {
-      res.push(path)
-      return
-    }
-    nums.forEach(n => {
-      if (path.includes(n)) {
-        return
-      }
-      backtrack(path.concat(n))
-    })
-  }
-  backtrack([])
+// var permute = function (nums) { 
+//   let res = []
+//   backtrack([])
+//   function backtrack(path) {
+//     if (path.length === nums.length) {
+//       res.push([...path])
+//       return
+//     }
+//     for (let i = 0; i< nums.length; i++) {
+//       if (path.includes(nums[i])) {
+//         continue
+//       }
+//       path.push(nums[i])
+//       backtrack(path)
+//       path.pop()
+//     }
+//   }
+//   return res
+// }
+
+const permute = (nums) => {
+  let len = nums.length, res = [], path = []
+  if (len === 0) return
+  dfs(nums, len, 0, path, res)
   return res
 }
 
+function dfs (nums, len, depth, path, res) {
+  if (depth === len) {
+    res.push([...path])
+    return
+  }
+  for (let i = 0; i < len; i++) {
+    if (path.includes(nums[i])) {
+      continue
+    }
+    path.push(nums[i])
+    dfs(nums, len, depth+1, path, res)
+    path.pop();
+  }
+}
 
-
-console.log(permute([1,1,2]))
+console.log(permute([1,2,3]))
--- leetcode/回溯/78-子集.js
+++ leetcode/回溯/78-子集.js
@@ -1,20 +1,36 @@
 // 用递归模拟出所有的情况
-// 保证接的数字都是后面的数字
+// 保证接的数字都是后面的数
 
-var subsets = function (nums) {
-  let res = []
-  const backtrack = (path, l, idx) => {
-    if (path.length === l) {
-      res.push(path)
-      return
-    }
-    for (let i = idx; i< nums.length; i++) {
-      backtrack(path.concat(nums[i]), l, i+1)
+// 方法一
+// var subsets = function (nums) {
+//   let res = []
+//   const backtrack = (path, l, idx) => {
+//     if (path.length === l) {
+//       res.push(path)
+//       return
+//     }
+//     for (let i = idx; i < nums.length; i++) {
+//       backtrack(path.concat(nums[i]), l, i+1)
+//     }
+//   }
+//   for (let i = 0; i <= nums.length; i++) {
+//     backtrack([], i, 0)
+//   }
+//   return res
+// };
+
+// 方法二
+const subsets = (nums) => {
+  const res = []
+  const dfs = (index, list) => {
+    res.push(list.slice())
+    for (let i = index; i < nums.length; i++) {
+      list.push(nums[i])
+      dfs(i+1, list)
+      list.pop()
     }
   }
-  for (let i = 0; i <= nums.length; i++) {
-    backtrack([], i, 0)
-  }
+  dfs(0, [])
   return res
-};
+}
 console.log(subsets([1,2,3]))
